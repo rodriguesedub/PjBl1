92f2b6585d03cfbc76465cbe7d36f394759ba9b2
rodriguesedub <rodrigues.epuc@gmail.com>
2025-09-09 15:49:07 -0300

    Create Pet.java

---
+++ Pet.java
@@ -0,0 +1,28 @@
+public class Dependente{
+    private String nome;
+    private String tipo;
+    public Pet(String nome, String tipo){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public String setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String setTipo(String tipo){
+        this.tipo = tipo
+    }
+
+    public String toString(){
+        return " - Nome do pet: "+nome+"; Tipo: "+tipo+".";
+    }
+}
