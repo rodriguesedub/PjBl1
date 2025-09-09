92a9e5bcccd249a101dca2b58b16292e93b40a85
rodriguesedub <rodrigues.epuc@gmail.com>
2025-09-09 18:58:53 -0300

    Update PetShop.java

--- PetShop.java
+++ PetShop.java
@@ -1,5 +1,41 @@
-public class PetShop{
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PetShop {
+    
+    private static ArrayList<Tutor> tutores;
+
     public static void main(String[] args) {
-        //Metodos
+        // controla menu (Scanner)
+    }
+
+    public static void popularCadastro() {
+        // insere tutores e pets iniciais
+    }
+
+    public static int geraCodTutor() {
+        // gera código sequencial automático
+        return 0;
+    }
+
+    public static void cadastrarTutorPets() {
+        // cadastra tutor + seus pets
+    }
+
+    public static void imprimirCadastro() {
+        // percorre lista de tutores, imprime toString()
+    }
+
+    public static void buscarPorCodigo(int cod) {
+        // procura tutor pelo código e exibe
+    }
+
+    public static void excluirPorCodigo(int cod) {
+        // remove tutor e seus pets
+    }
+
+    private static boolean validaData(int d, int m, int a) {
+        // valida datas de nascimento
+        return false;
     }
 }
