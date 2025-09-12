e2e894d1a5553faa0fd588489fb398826177648a
LeoPedriali5 <leopedriali@gmail.com>
2025-09-12 11:37:43 -0300

    Update Tutor.java

--- Tutor.java
+++ Tutor.java
@@ -29,17 +29,14 @@ public class Tutor{
     }
 
     public String getIdade(){
-        //TODO: calcula idade com Period.between(dataNasc, LocalDate.now())
         return Period.between(dataNasc, LocalDate.now()).getYears();
     }
 
     public void add(Pet p){
-        //TODO: Escrever a função
         this.pets.add(p);
     }
 
     public void removePet(Pet p){
-        //TODO: Escrever a função
         this.pets.remove(p);
     }
 
@@ -49,7 +46,6 @@ public class Tutor{
                    "Data nascimento: " + getDataNasc() + " (" + getIdade() + " anos)\n" +
                    "Endereço......: " + getEndereco() + "\n" +
                    "Relação de Pets:\n";
-        //TODO: Escrever a saida de relacao Pets
         if (pets.isEmpty()){
             text += "  Nenhum pet cadastrado.";
         } else {
