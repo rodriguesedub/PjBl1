674b60a61e995243e176f0363b419358d2c94b65
rodriguesedub <rodrigues.epuc@gmail.com>
2025-09-18 14:26:51 -0300

    Update Tutor.java

--- Tutor.java
+++ Tutor.java
@@ -3,14 +3,15 @@ import java.util.ArrayList;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
-public class Tutor{
-    private int cod;
+public class Tutor {
+    private int cod;                // código único do tutor
     private String nome;
     private String endereco;
-    private LocalDate dataNasc;
-    private ArrayList<Pet> pets;
+    private LocalDate dataNasc;     // data de nascimento do tutor
+    private ArrayList<Pet> pets;    // lista de pets associados ao tutor
 
-    public Tutor(int cod, String nome, LocalDate dataNasc, String endereco){
+    // construtor
+    public Tutor(int cod, String nome, LocalDate dataNasc, String endereco) {
         this.cod = cod;
         this.nome = nome;
         this.dataNasc = dataNasc;
@@ -18,45 +19,50 @@ public class Tutor{
         this.pets = new ArrayList<>();
     }
 
-    public int getCod(){
+    public int getCod() {
         return cod;
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
 
-    public String getEndereco(){
+    public String getEndereco() {
         return endereco;
     }
 
-    public LocalDate getDataNasc(){
+    public LocalDate getDataNasc() {
         return dataNasc;
     }
 
-    public int getIdade(){
+    // calcula idade do tutor
+    public int getIdade() {
         return Period.between(dataNasc, LocalDate.now()).getYears();
     }
 
-    public void add(Pet p){
+    // adiciona pet à lista
+    public void add(Pet p) {
         this.pets.add(p);
     }
 
-    public void removePet(Pet p){
+    // remove pet da lista
+    public void removePet(Pet p) {
         this.pets.remove(p);
     }
 
-    public String toString(){
+    // representação em string do tutor e seus pets
+    public String toString() {
         DateTimeFormatter dataBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String text = "Cod. do tutor: " + getCod() + "\n" +
                    "Nome...........: " + getNome() + "\n" +
                    "Data nascimento: " + getDataNasc().format(dataBR) + " (" + getIdade() + " anos)\n" +
                    "Endereço.......: " + getEndereco() + "\n" +
                    "Relação de Pets:\n";
-        if (pets.isEmpty()){
+
+        if (pets.isEmpty()) {
             text += "  Nenhum pet cadastrado.";
         } else {
-            for (Pet pet : pets){
+            for (Pet pet : pets) {
                 text += "- Nome do pet: " + pet.getNome() + "; Tipo: " + pet.getTipo() + ".\n";
             }
         }
